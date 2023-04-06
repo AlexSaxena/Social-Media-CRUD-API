4f2028a8c3c63b38c21bd191dbd874eb9c9fdc10
AlexSaxena <saxenaalex@gmail.com>
2023-04-06 22:01:35 +0200

    Adds Dependencies to index

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("General Kenobi");
+
+// Dependencies
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
