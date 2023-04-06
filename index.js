ea3fa1ec7b7f264a22054704321890209196cdde
AlexSaxena <saxenaalex@gmail.com>
2023-04-06 22:02:09 +0200

    Enables Cors, Cookies & JSON to be read/used

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const app = express();
+
+// Enables Cors, Cookies & JSON to be read/used
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
