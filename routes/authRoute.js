c55b963bdcdfe9452f82992a7abe5a459fe332f7
AlexSaxena <saxenaalex@gmail.com>
2023-04-11 00:45:19 +0200

    Moves authController to authController directory

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { login } = require("../controllers/authController");
+const { login } = require("../controllers/userAuthControllers/authController");
 const { register } = require("../controllers/userAuthControllers/register");
 
 const authRoute = express.Router();
