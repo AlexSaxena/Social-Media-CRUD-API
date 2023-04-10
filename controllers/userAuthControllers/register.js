a935de1518033f2c7281e19635c08c15208859d6
AlexSaxena <saxenaalex@gmail.com>
2023-04-11 00:26:17 +0200

    Adds userRegistration Validation Exist,uName,uPass

--- controllers/userAuthControllers/register.js
+++ controllers/userAuthControllers/register.js
@@ -1,22 +1,38 @@
 const { connect } = require("../../db/connect");
+const { loginSchema, checkUserExists } = require("../../model/authModel");
 
 const register = async function (req, res) {
+  let validation = loginSchema.validate(req.body);
+  if (validation.error) {
+    return res
+      .json({ message: validation.error.details[0].message })
+      .status(406);
+  }
+
   const { username, password } = req.body;
+  const checkUser = await checkUserExists(username);
 
-  let newUserData = {
-    username: username,
-    password: password,
-    date,
-    following: [],
-  };
+  if (checkUser) {
+    return res.status(409).json({ message: "User Already Exists!" });
+  } else {
+    let newUserData = {
+      username: username,
+      password: password,
+      date,
+      following: [],
+    };
 
-  const db = await connect();
-  const collection = db.collection("users");
+    const db = await connect();
+    const collection = db.collection("users");
 
-  const newUser = await collection.insertOne(newUserData, function (err, res) {
-    if (err) res.json({ message: err }).status(400);
-  });
-  return res.json({ message: "New User Added" }).status(200);
+    const newUser = await collection.insertOne(
+      newUserData,
+      function (err, res) {
+        if (err) res.json({ message: err }).status(400);
+      }
+    );
+    return res.json({ message: "New User Added" }).status(200);
+  }
 };
 
 let date = new Date();
