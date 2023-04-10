e6f9d751fc90ead08fc5059440dea10bf2684831
AlexSaxena <saxenaalex@gmail.com>
2023-04-11 00:38:43 +0200

    comments test WIP

--- tests/signup.test.js
+++ tests/signup.test.js
@@ -1,15 +1,13 @@
-describe("Test user sign up", () => {
+describe("Test user Register", () => {
   it("Sanity Check", async () => {
     //Test Go Here
     expect(1 + 1).toBe(2);
   });
-  it("Verify if a user can Sign Up with valid username and password.", async () => {
-    //Test Go Here
-  });
-  it("Sign Up Username must be over 3 characters long", async () => {
-    //Test Go Here
-  });
-  it("Sign Up Password must be over 3 characters long", async () => {
-    //Test Go Here
-  });
+  //   it("Register Username must be over 3 characters long", async () => {
+  //     //Test Go Here
+  //     let username = register.req.body.username
+  //   });
+  //   it("Register Password must be over 3 characters long", async () => {
+  //     //Test Go Here
+  //   });
 });
