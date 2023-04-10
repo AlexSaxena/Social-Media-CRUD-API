a438d23c61b9dea72259a321b4602923bee27d7e
AlexSaxena <saxenaalex@gmail.com>
2023-04-10 23:42:05 +0200

    Adds db connection & insert functionality for user

--- controllers/userAuthControllers/register.js
+++ controllers/userAuthControllers/register.js
@@ -1,7 +1,24 @@
-require("dotenv").config();
+const { connect } = require("../../db/connect");
 
-const register = function (req, res) {
-  console.log("register Controller");
+const register = async function (req, res) {
+  const { username, password } = req.body;
+
+  let newUserData = {
+    username: username,
+    password: password,
+    date,
+    following: [],
+  };
+
+  const db = await connect();
+  const collection = db.collection("users");
+
+  const newUser = await collection.insertOne(newUserData, function (err, res) {
+    if (err) res.json({ message: err }).status(400);
+  });
+  return res.json({ message: "New User Added" }).status(200);
 };
+
+let date = new Date();
 
 exports.register = register;
