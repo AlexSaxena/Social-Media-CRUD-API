b2e7449da2df9c3451cc159a7dae25e360550ffc
AlexSaxena <saxenaalex@gmail.com>
2023-04-11 17:57:36 +0200

    Adds route and express.Json to test

--- controllers/userPostControllers/deletePostController.js
+++ controllers/userPostControllers/deletePostController.js
@@ -1,30 +1,32 @@
-const {ObjectId} = require("mongodb");
-const {connect} = require("../../db/connect");
-const {deleteSchema, checkPostExist} = require("../../model/postModel");
+const { ObjectId } = require("mongodb");
+const { connect } = require("../../db/connect");
+const { deleteSchema, checkPostExist } = require("../../model/postModel");
 
 const deletePost = async function (req, res) {
   let validation = deleteSchema.validate(req.body);
   if (validation.error) {
-    return res.json({message: validation.error.details[0].message}).status(406);
+    return res
+      .status(406)
+      .json({ message: validation.error.details[0].message });
   }
 
-  const {id} = req.body;
+  const { id } = req.body;
 
   const checkPost = await checkPostExist(id);
 
   if (checkPost === false) {
-    return res.status(409).json({message: "Post doesn't exist"});
+    return res.status(409).json({ message: "Post doesn't exist" });
   } else {
     try {
       const db = await connect();
       const collection = db.collection("posts");
 
-      await collection.deleteOne({_id: new ObjectId(id)});
+      await collection.deleteOne({ _id: new ObjectId(id) });
 
-      return res.status(200).json({message: "Post deleted successfully"});
+      return res.status(200).json({ message: "Post deleted successfully" });
     } catch (err) {
       console.error(err);
-      return res.status(400).json({message: "Error deleting post"});
+      return res.status(400).json({ message: "Error deleting post" });
     }
   }
 };
--- tests/postRoute.test.js
+++ tests/postRoute.test.js
@@ -1,12 +1,17 @@
 const request = require("supertest");
 const express = require("express");
+const { postRoute } = require("../routes/postRoute");
 
 const app = express();
+app.use(express.json());
+app.use("/posts", postRoute);
 
 describe("DELETE /posts/delete", () => {
   it("should return status 409 for an invalid post ID", async () => {
     // Send a delete request to the /posts/delete endpoint with an invalid ID
-    const response = await request(app).delete("/posts/delete").send({id: "6433c1c848726936a72bea2e"});
+    const response = await request(app)
+      .delete("/posts/delete")
+      .send({ id: "6433c1c848726936a72bea2e" });
 
     // Check that the response has a status of 409
     expect(response.status).toBe(409);
