14a441eff37109624a6780d963048a03b11fc21b
arpg <araceli.prietogallego@yh.nackademin.se>
2023-04-11 21:27:54 +0200

    added getAllPosts to postRoute

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { createPost } = require('../controllers/postControllers/createPost');
+const { getAllPosts } = require('../controllers/postControllers/getAllPosts');
 const {deletePost} = require('../controllers/userPostControllers/deletePostController');
 
 const postRoute = express.Router();
 
 postRoute.post('/create', createPost)
+postRoute.get('/all', getAllPosts);
 // DELETE route
 postRoute.delete('/', deletePost);
 
