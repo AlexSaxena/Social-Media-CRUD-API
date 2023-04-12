20373f678dbb1789680dd4c467a95ee49d34f68a
KennethEllebring <kenneth.ellebring@yh.nackademin.se>
2023-04-12 08:32:48 +0200

    fixxed tests

--- tests/postRoute.test.js
+++ tests/postRoute.test.js
@@ -6,14 +6,14 @@ const app = express();
 app.use(express.json());
 app.use('/posts', postRoute);
 
-describe('DELETE /posts/delete', () => {
+describe('DELETE /posts', () => {
   it('should return status 406 if there is no ID', async () => {
-    const response = await request(app).delete('/posts/delete').send({id: ''});
+    const response = await request(app).delete('/posts').send({id: ''});
     expect(response.status).toBe(406);
   });
 
   it('should return ""id" length must be 24 characters long"', async () => {
-    const response = await request(app).delete('/posts/delete').send({id: '6433c1c8487'});
+    const response = await request(app).delete('/posts').send({id: '6433c1c8487'});
     expect(response.body.message).toBe('"id" length must be 24 characters long');
   });
 });
