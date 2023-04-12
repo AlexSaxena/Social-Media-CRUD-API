28a79a1cc4c4477bf39726bf173f12244c5092e4
AlexSaxena <saxenaalex@gmail.com>
2023-04-12 23:41:54 +0200

    Adds Logout Route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { login } = require("../controllers/userAuthControllers/loginController");
 const {
+  logout,
+} = require("../controllers/userAuthControllers/logoutController");
+const {
   register,
 } = require("../controllers/userAuthControllers/registerController");
 
@@ -8,6 +11,7 @@ const authRoute = express.Router();
 
 authRoute.post("/login", login);
 authRoute.post("/register", register);
+authRoute.get("/logout", logout);
 
 module.exports = {
   authRoute,
