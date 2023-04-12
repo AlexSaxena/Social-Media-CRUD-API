efc0573b4ae6fd65aaeef09e54455842d1ee8abe
arpg <araceli.prietogallego@yh.nackademin.se>
2023-04-12 13:35:49 +0200

    gets username from JWT instead of request body

--- controllers/postControllers/createPost.js
+++ controllers/postControllers/createPost.js
@@ -6,8 +6,9 @@ exports.createPost = async function createPost(req, res) {
     const { error } = postSchema.validate(req.body);
 
     if(error) return res.status(400).json({ message: error.details[0].message });
-
-    const { body, userID } = req.body;
+    
+    const { body } = req.body;
+    const userID = req.loggedInUser.user;
     const date = new Date();
 
     const newPost = {
@@ -16,14 +17,14 @@ exports.createPost = async function createPost(req, res) {
         date: date,
         like: [],
         userID: userID
-    }
+    };
 
     const db = await connect();
     const collection = db.collection('posts');
 
     collection.insertOne(newPost, function (err, res) {
         if(err) return res.status(500).json({ message: err });
-    })
+    });
 
     res.status(201).json({ message: 'Successfully created post!' });
-  }
+  };
