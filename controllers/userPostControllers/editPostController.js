d44a49b411557db830c2d39ffe8e01707b912e6d
AlexSaxena <saxenaalex@gmail.com>
2023-04-12 16:16:56 +0200

    Adds Patch Functionality to post

--- controllers/userPostControllers/editPostController.js
+++ controllers/userPostControllers/editPostController.js
@@ -18,15 +18,28 @@ const editPost = async function (req, res) {
 
   const { id, body } = req.body;
   const { user } = req.loggedInUser;
-  console.log(id, body, user);
+  console.log("id ->" + id, "Body ->" + body, "User ->" + user);
 
   const checkPost = await checkPostExist(id, user);
 
   if (checkPost === false) {
     return res.status(404).json({ message: "Post doesn't exist" });
-  }
+  } else {
+    const db = await connect();
+    const collection = db.collection("posts");
 
-  res.status(200).json({ id: "Id is ->" + id, body: body });
+    await collection.updateOne(
+      { _id: new ObjectId(id), userID: user },
+      {
+        $set: {
+          body: body,
+        },
+      },
+      { $currentDate: { lastUpdated: true } }
+    );
+
+    return res.status(200).json({ message: "Post updated successfully" });
+  }
 };
 
 exports.editPost = editPost;
