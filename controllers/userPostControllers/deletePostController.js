057a0b072e02c714bd870463ae7eb1fcdb571724
KennethEllebring <kenneth.ellebring@yh.nackademin.se>
2023-04-12 08:32:08 +0200

    changed statuscode to be more accurate

--- controllers/userPostControllers/deletePostController.js
+++ controllers/userPostControllers/deletePostController.js
@@ -1,32 +1,30 @@
-const { ObjectId } = require("mongodb");
-const { connect } = require("../../db/connect");
-const { deleteSchema, checkPostExist } = require("../../model/postModel");
+const {ObjectId} = require('mongodb');
+const {connect} = require('../../db/connect');
+const {deleteSchema, checkPostExist} = require('../../model/postModel');
 
 const deletePost = async function (req, res) {
   let validation = deleteSchema.validate(req.body);
   if (validation.error) {
-    return res
-      .status(406)
-      .json({ message: validation.error.details[0].message });
+    return res.status(406).json({message: validation.error.details[0].message});
   }
 
-  const { id } = req.body;
+  const {id} = req.body;
 
   const checkPost = await checkPostExist(id);
 
   if (checkPost === false) {
-    return res.status(409).json({ message: "Post doesn't exist" });
+    return res.status(404).json({message: "Post doesn't exist"});
   } else {
     try {
       const db = await connect();
-      const collection = db.collection("posts");
+      const collection = db.collection('posts');
 
-      await collection.deleteOne({ _id: new ObjectId(id) });
+      await collection.deleteOne({_id: new ObjectId(id)});
 
-      return res.status(200).json({ message: "Post deleted successfully" });
+      return res.status(200).json({message: 'Post deleted successfully'});
     } catch (err) {
       console.error(err);
-      return res.status(400).json({ message: "Error deleting post" });
+      return res.status(400).json({message: 'Error deleting post'});
     }
   }
 };
