e9b5ac877bebbc69cb079cee7ee42047882d4dec
Linus Haren <linus@haren.io>
2023-04-12 15:21:45 +0200

    add /follow endpoint on user route. Used with patch request to follow a user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { getAllUsers } = require("../controllers/userControllers/getUsersController");
+const { followUser } = require('../controllers/userControllers/followUserController')
 
 const userRoute = express.Router();
 
 userRoute.get("/all", getAllUsers);
+userRoute.patch("/follow", followUser);
+
 
 module.exports = {
   userRoute,
