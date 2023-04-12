8c38853d4192af9404c051b1fad28b4828f96243
Linus Haren <linus@haren.io>
2023-04-12 15:59:19 +0200

    Add following endpoint

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { getAllUsers } = require("../controllers/userControllers/getUsersController");
 const { followUser } = require('../controllers/userControllers/followUserController')
+const { getUserFollowing } = require('../controllers/userControllers/getFollowingListController')
 
 const userRoute = express.Router();
 
 userRoute.get("/all", getAllUsers);
 userRoute.patch("/follow", followUser);
+userRoute.get('/following', getUserFollowing)
 
 
 module.exports = {
