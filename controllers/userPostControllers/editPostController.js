32b22fae919f574165ad88b99abdc7e658fb16b1
AlexSaxena <saxenaalex@gmail.com>
2023-04-12 16:19:55 +0200

    Removes Comments & ConsoleLogs

--- controllers/userPostControllers/editPostController.js
+++ controllers/userPostControllers/editPostController.js
@@ -1,12 +1,6 @@
-// Add dependancy
-// Add Validation
-// Check post exist
-// If Doesn't Exist send error msg
-// If exist let user edit post
 const { ObjectId } = require("mongodb");
 const { connect } = require("../../db/connect");
 const { IDCheckSchema, checkPostExist } = require("../../model/postModel");
-// Current user - token
 
 const editPost = async function (req, res) {
   let validation = IDCheckSchema.validate(req.body);
@@ -18,7 +12,6 @@ const editPost = async function (req, res) {
 
   const { id, body } = req.body;
   const { user } = req.loggedInUser;
-  console.log("id ->" + id, "Body ->" + body, "User ->" + user);
 
   const checkPost = await checkPostExist(id, user);
 
