6c8ef06a016dbc3546fed4f394ed43409c0c2b12
AlexSaxena <saxenaalex@gmail.com>
2023-04-13 12:21:28 +0200

    Adds validation to check req ID

--- controllers/postControllers/getSinglePostController.js
+++ controllers/postControllers/getSinglePostController.js
@@ -1,4 +1,19 @@
+const { ObjectId } = require("mongodb");
+const { connect } = require("../../db/connect");
+const { deleteSchema } = require("../../model/postModel");
+// Same Model as deleteSchema Can be changed Later
+
 const singlePost = async (req, res) => {
+  // deletSchema can be changed later
+  let validation = deleteSchema.validate(req.body);
+  if (validation.error) {
+    return res
+      .status(406)
+      .json({ message: validation.error.details[0].message });
+  }
+
+  const { id } = req.body;
+
   res.send("Hello from SinglePost Controller");
 };
 
