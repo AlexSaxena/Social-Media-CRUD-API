f1b37551ff245684994d81ffd8475e98f3fdbb0e
KennethEllebring <kenneth.ellebring@yh.nackademin.se>
2023-04-13 18:13:17 +0200

    added getallposts route

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -4,16 +4,17 @@ const {createPost} = require('../controllers/postControllers/createPostControlle
 const {getAllPosts} = require('../controllers/postControllers/getAllPosts');
 const {deletePost} = require('../controllers/postControllers/deletePostController');
 const {editPost} = require('../controllers/postControllers/editPostController');
-const { singlePost } = require("../controllers/postControllers/getSinglePostController");
+const {singlePost} = require('../controllers/postControllers/getSinglePostController');
+const {getAllUserPosts} = require('../controllers/postControllers/getAllUserPosts');
 
 const postRoute = express.Router();
 
 postRoute.post('/', createPost);
 postRoute.get('/all', getAllPosts);
-postRoute.get("/post/", singlePost);
+postRoute.get('/all/user', getAllUserPosts);
+postRoute.get('/post/', singlePost);
 postRoute.patch('/', editPost);
 postRoute.delete('/', deletePost);
-
 
 module.exports = {
   postRoute,
