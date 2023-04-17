f02d7517de92f669e03c4c42ea5ec11b5a2d4563
AlexSaxena <saxenaalex@gmail.com>
2023-04-17 11:34:42 +0200

    Adds comment functionality

--- controllers/postControllers/commentOnPostController.js
+++ controllers/postControllers/commentOnPostController.js
@@ -2,20 +2,29 @@ const { ObjectId } = require("mongodb");
 const { getClientDB } = require("../../db/connect");
 
 const commentOnPost = async function (req, res) {
-  const { id, body } = req.body;
+  const { id, commentBody } = req.body;
 
   // Checks DB for desired post
   const db = await getClientDB();
   const postCollection = db.collection("posts");
   const selectedPost = await postCollection.findOne({ _id: new ObjectId(id) });
 
-  if (selectedPost === null)
+  if (selectedPost === null || selectedPost === undefined)
     return res
       .status(404)
       .json({ message: "Something Went Wrong finding Post." });
 
-  // WIP Check Db connection & Post Will be Remmoved
-  res.json({ post: selectedPost });
+  await postCollection.updateOne(
+    { _id: new ObjectId(id) },
+    {
+      $push: {
+        comments: commentBody,
+      },
+    },
+    { $currentDate: { lastUpdated: true } }
+  );
+
+  res.json({ message: "New Comment Added To Post" });
 };
 
 exports.commentOnPost = commentOnPost;
