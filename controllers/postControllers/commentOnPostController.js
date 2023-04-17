d1c29b85df81229c8ff48d482f0b1af74bb55da4
AlexSaxena <saxenaalex@gmail.com>
2023-04-17 11:22:54 +0200

    Adds connection to DB

--- controllers/postControllers/commentOnPostController.js
+++ controllers/postControllers/commentOnPostController.js
@@ -1,8 +1,21 @@
-const {getClientDB} = require('../../db/connect');
+const { ObjectId } = require("mongodb");
+const { getClientDB } = require("../../db/connect");
 
+const commentOnPost = async function (req, res) {
+  const { id, body } = req.body;
 
-const commentOnPost = async function (req,res) {
-        
-}
+  // Checks DB for desired post
+  const db = await getClientDB();
+  const postCollection = db.collection("posts");
+  const selectedPost = await postCollection.findOne({ _id: new ObjectId(id) });
+
+  if (selectedPost === null)
+    return res
+      .status(404)
+      .json({ message: "Something Went Wrong finding Post." });
+
+  // WIP Check Db connection & Post Will be Remmoved
+  res.json({ post: selectedPost });
+};
 
 exports.commentOnPost = commentOnPost;
