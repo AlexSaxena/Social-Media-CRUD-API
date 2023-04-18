dffaca43477853c7b640c10dc1029554ed930725
Linus Haren <linus@haren.io>
2023-04-18 16:50:23 +0200

    change getAllUserPosts route to expect POST instead of GET

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -15,7 +15,7 @@ postRoute.post('/', createPost);
 postRoute.patch('/', editPost);
 postRoute.delete('/', deletePost);
 postRoute.get('/all', getAllPosts);
-postRoute.get('/all/user', getAllUserPosts);
+postRoute.post('/all/user', getAllUserPosts);
 postRoute.get('/post/', singlePost);
 postRoute.patch('/post/', commentOnPost);
 postRoute.patch('/like', likePost);
