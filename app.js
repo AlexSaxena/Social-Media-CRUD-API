9ebeef60d932417e655f0971b57e6802165628e6
AlexSaxena <saxenaalex@gmail.com>
2023-04-26 10:48:11 +0200

    removes not needed /

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://penfriend.onrender.com/"],
+    origin: ["http://localhost:3000", "https://penfriend.onrender.com"],
     credentials: true,
   })
 );
