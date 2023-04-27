2f9ccd008b8445ae8504efea40c9b3fdd3ed5ba4
KennethEllebring <kenneth.ellebring@yh.nackademin.se>
2023-04-27 09:37:21 +0200

    added info to cookie

--- controllers/userAuthControllers/loginController.js
+++ controllers/userAuthControllers/loginController.js
@@ -1,46 +1,44 @@
-const { loginSchema, checkUserExists } = require("../../model/authModel");
-const bcrypt = require("bcrypt");
-const dotenv = require("dotenv").config({ path: "../../config/.env" });
-const jwt = require("jsonwebtoken");
+const {loginSchema, checkUserExists} = require('../../model/authModel');
+const bcrypt = require('bcrypt');
+const dotenv = require('dotenv').config({path: '../../config/.env'});
+const jwt = require('jsonwebtoken');
 
 const generateAccessToken = (username) => {
-  return jwt.sign({ user: username }, process.env.TOKEN_SECRET, {
-    expiresIn: "1h",
+  return jwt.sign({user: username}, process.env.TOKEN_SECRET, {
+    expiresIn: '1h',
   });
 };
 
 const login = async (req, res) => {
   try {
-    const { error, value } = loginSchema.validate(req.body);
+    const {error, value} = loginSchema.validate(req.body);
     if (error) {
       const errorMessage = error.details[0].message;
-      return res.status(400).json({ message: errorMessage });
+      return res.status(400).json({message: errorMessage});
     } else {
-      const { username, password } = value;
+      const {username, password} = value;
 
       const user = await checkUserExists(username);
       if (!user) {
-        return res
-          .status(401)
-          .json({ message: "Login failed: Invalid credentials" });
+        return res.status(401).json({message: 'Login failed: Invalid credentials'});
       } else {
         if (await bcrypt.compare(password, user.password)) {
           const accessToken = generateAccessToken(user.username);
           return res
-            .cookie("authToken", accessToken, {
+            .cookie('authToken', accessToken, {
               httpOnly: true,
+              sameSite: 'none',
+              secure: true,
             })
             .status(200)
-            .json({ message: "Login successful" });
+            .json({message: 'Login successful'});
         } else {
-          return res
-            .status(401)
-            .json({ message: "Login failed: Invalid credentials" });
+          return res.status(401).json({message: 'Login failed: Invalid credentials'});
         }
       }
     }
   } catch (err) {
-    return res.status(500).json({ message: "Internal server error" });
+    return res.status(500).json({message: 'Internal server error'});
   }
 };
 
